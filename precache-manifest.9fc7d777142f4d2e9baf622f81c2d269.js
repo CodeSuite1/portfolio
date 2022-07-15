afff89248ebc8cab8f565c747302751383f8be3c
Jawwad Ahmed <jawadah303@gmail.com>
2022-07-15 22:03:49 +0500

    Updates

---
+++ precache-manifest.9fc7d777142f4d2e9baf622f81c2d269.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7dda0d03fd9febfd481",
+    "url": "/portfolio/static/css/main.06f270e7.chunk.css"
+  },
+  {
+    "revision": "f7dda0d03fd9febfd481",
+    "url": "/portfolio/static/js/main.f7dda0d0.chunk.js"
+  },
+  {
+    "revision": "24c21998ad8143be683f",
+    "url": "/portfolio/static/css/1.590a7d42.chunk.css"
+  },
+  {
+    "revision": "24c21998ad8143be683f",
+    "url": "/portfolio/static/js/1.24c21998.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "23273bc8bc3197e29e433d7cf66185fc",
+    "url": "/portfolio/static/media/sklearn_genetic_opt.23273bc8.png"
+  },
+  {
+    "revision": "fed0850a36ae8cfbe9ceaa1ce970c5cc",
+    "url": "/portfolio/static/media/graph_embeddings.fed0850a.png"
+  },
+  {
+    "revision": "9c0f986795edbdc96c99bc8853580310",
+    "url": "/portfolio/static/media/kafkaml.9c0f9867.png"
+  },
+  {
+    "revision": "8ba027d94d0cd4d31608e2c6e43f9488",
+    "url": "/portfolio/static/media/scikit_pipes.8ba027d9.png"
+  },
+  {
+    "revision": "4b632d51a45152af9b70077a3d4955ca",
+    "url": "/portfolio/static/media/portfolio.4b632d51.png"
+  },
+  {
+    "revision": "f7d742fc14d3d495da2581043372d15a",
+    "url": "/portfolio/static/media/fastapi_sklearn.f7d742fc.png"
+  },
+  {
+    "revision": "6c4aa8989deec4331f47e5e64bd50f38",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.96bb6fb39646043bb4399db0fdee5a58.js"
+  "/portfolio/precache-manifest.9fc7d777142f4d2e9baf622f81c2d269.js"
 );
 
 workbox.clientsClaim();
